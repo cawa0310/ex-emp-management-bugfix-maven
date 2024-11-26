c165b7acc3f648acc3bf14d9245f078eb7ea6bdc
cawa0310 <cawaguchi@icloud.com>
2024-11-26 11:36:46 +0900

    bugfix/4-3_gapInit

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,7 +1,5 @@
 package com.example.form;
 
-import org.springframework.validation.annotation.Validated;
-
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
